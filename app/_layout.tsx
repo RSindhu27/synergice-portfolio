b99a1963c37bc65a2bb31976de019336517720c5
RSindhu27 <sindhu.mr@neviton.com>
2026-06-24 12:25:47 +0530

    Updated tsconfig.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
